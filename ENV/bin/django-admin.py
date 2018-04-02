b5fdedce9d14e5235c7371ef9771841b065b4191
Jessica Ng <jessica.hl.ng5@gmail.com>
2018-04-02 18:32:28 -0400

    virtualenv

---
+++ ENV/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/jessica_ng/all_the_stuff/MHC/SPRING1718/Big_Data/Spring2018BigDataProject1/ENV/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
