ad2624ad73132a7798a435da65a331a286a1a713
Eunice <eunicehew97@gmail.com>
2018-04-03 14:34:11 -0400

    remove virtualenv

--- ENV/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/jessica_ng/all_the_stuff/MHC/SPRING1718/Big_Data/Spring2018BigDataProject1/ENV/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
